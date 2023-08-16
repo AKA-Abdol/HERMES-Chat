6ff7104d483bb7f1cbc18198c4f74726520febaa
aka-abdol <thegrolam@gmail.com>
2023-08-16 09:49:20 +0330

    feat: UserAuthenticationService refactored

--- src/main/java/com/codestar/HAMI/service/UserAuthenticationService.java
+++ src/main/java/com/codestar/HAMI/service/UserAuthenticationService.java
@@ -2,20 +2,26 @@ package com.codestar.HAMI.service;
 
 import com.codestar.HAMI.entity.Profile;
 import com.codestar.HAMI.entity.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserAuthenticationService {
     public User getAuthenticatedUser() {
-        return (User) SecurityContextHolder
+        User user = (User) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
+        if (user == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found!");
+
+        return user;
     }
 
     public Profile getAuthenticatedProfile() {
         User user = getAuthenticatedUser();
-        return user.getProfile();
+        return user.getProfile(); // when a user is valid but profile not found!
     }
 }
