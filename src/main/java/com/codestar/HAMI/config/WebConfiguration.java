e9efdfd6dd3450fca144d1f8e9864cda33e1ccfd
aka-abdol <thegrolam@gmail.com>
2023-08-17 10:49:39 +0330

    fix: cors problem resolved.

--- src/main/java/com/codestar/HAMI/config/WebConfiguration.java
+++ src/main/java/com/codestar/HAMI/config/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTION")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
                 .allowedOrigins("*");
     }
 }
